2487ac36dca7913efdf67a6fc35d87562e3ab9bb
Doojin Kang <doojin.kang@gmail.com>
2017-08-12 05:50:50 +0900

    Single books array

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const bookshelfReading = [
+const BOOK_READING =	  0
+const BOOK_WANNA_READ =	1
+const BOOK_READ	=	      2
+const BOOKSHELF_TITLES = ['Currently Reading', 'Want to Read', 'Read' ]
+
+const books = [
 	{
+		shelf: BOOK_READING,
 		cover: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
 		width: 128,
 		height: 193,
@@ -10,16 +16,15 @@ const bookshelfReading = [
 		author: "Harper Lee"
 	},
 	{
+		shelf: BOOK_READING,
 		cover: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
 		width: 128,
 		height: 188,
 		title: "Ender's Game",
 		author: "Orson Scott Card"
-	}
-]
-
-const bookshelfWant = [
+	},
 	{
+		shelf: BOOK_WANNA_READ,
 		cover: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
 		width: 128,
 		height: 193,
@@ -27,16 +32,15 @@ const bookshelfWant = [
 		author: "David McCullough"
 	},
 	{
+		shelf: BOOK_WANNA_READ,
 		cover: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
 		width: 128,
 		height: 192,
 		title: "Harry Potter and the Sorcerer's Stone",
 		author: "J.K. Rowling"
-	}
-]
-
-const bookshelfRead = [
+	},
 	{
+		shelf: BOOK_READ,
 		cover: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
 		width: 128,
 		height: 192,
@@ -44,6 +48,7 @@ const bookshelfRead = [
 		author: "J.R.R. Tolkien"
 	},
 	{
+		shelf: BOOK_READ,
 		cover: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
 		width: 128,
 		height: 174,
@@ -51,6 +56,7 @@ const bookshelfRead = [
 		author: "Seuss"
 	},
 	{
+		shelf: BOOK_READ,
 		cover: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
 		width: 128,
 		height: 192,
@@ -58,6 +64,7 @@ const bookshelfRead = [
 		author: "Mark Twain"
 	}
 ]
+
 
 class ListBooks extends Component {
 
@@ -74,7 +81,8 @@ class ListBooks extends Component {
 								<div className="bookshelf-books">
 									<ol className="books-grid">
 
-										{bookshelfReading.map( (book) => (
+										{books.filter( (book) => (book.shelf === BOOK_READING))
+                          .map( (book) => (
 											<li>
 												<div className="book">
 													<div className="book-top">
@@ -98,12 +106,14 @@ class ListBooks extends Component {
 									</ol>
 								</div>
 							</div>
+
 							<div className="bookshelf">
 								<h2 className="bookshelf-title">Want to Read</h2>
 								<div className="bookshelf-books">
 									<ol className="books-grid">
 
-										{bookshelfWant.map( (book) => (
+										{books.filter( (book) => (book.shelf === BOOK_WANNA_READ))
+                          .map( (book) => (
 											<li>
 												<div className="book">
 													<div className="book-top">
@@ -132,7 +142,8 @@ class ListBooks extends Component {
 								<div className="bookshelf-books">
 									<ol className="books-grid">
 
-										{bookshelfRead.map( (book) => (
+										{books.filter( (book) => (book.shelf === BOOK_READ))
+                          .map( (book) => (
 											<li>
 												<div className="book">
 													<div className="book-top">
